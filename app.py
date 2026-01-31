e1f8dba529a68e66479b6738b00af2acd63d3d00
keerthi <keerthiyogesh771@gmail.com>
2026-01-31 12:21:18 +0530

    hello

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
     
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
